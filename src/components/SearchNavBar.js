124a1ee5866f8cf4dc18a476c6c33cef25a3f9db
lennart <lsboecken@gmail.com>
2021-10-29 18:19:48 -0400

    fix : carry-over problem fix from past merge

--- src/components/SearchNavBar.js
+++ src/components/SearchNavBar.js
@@ -1,51 +1,25 @@
 import React from 'react';
 
 function SearchNavBar(props) {
-  if (props.isAxiosOrVanilla === 'vanilla') {
-    return (
-      <ul>
-        <li>
-          <button
-            onClick={() => {
-              props.onClickVanilla(null, props.previous);
-            }}>
-            Previous
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onClickVanilla(null, props.next);
-            }}>
-            Next
-          </button>
-        </li>
-      </ul>
-    );
-  }
-
-  if (props.isAxiosOrVanilla === 'axios') {
-    return (
-      <ul>
-        <li>
-          <button
-            onClick={() => {
-              props.onClickAxios(null, props.previous);
-            }}>
-            Previous
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={() => {
-              props.onClickAxios(null, props.next);
-            }}>
-            Next
-          </button>
-        </li>
-      </ul>
-    );
-  }
+  return (
+    <ul>
+      <li>
+        <button
+          onClick={() => {
+            props.onClick(null, props.previous);
+          }}>
+          Previous
+        </button>
+      </li>
+      <li>
+        <button
+          onClick={() => {
+            props.onClick(null, props.next);
+          }}>
+          Next
+        </button>
+      </li>
+    </ul>
+  );
 }
-
 export default SearchNavBar;
