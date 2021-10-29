2bd80132ce6a6cd000f1082a04d48373f0a4891c
lennart <lsboecken@gmail.com>
2021-10-29 18:12:12 -0400

    resolve merge conflict from master & refactor2

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -5,50 +5,6 @@ function InputForm(props) {
     props.updateSearchTerm(value);
   }
 
-<<<<<<< HEAD
-  if (props.axiosOrVanilla === 'vanilla') {
-    return (
-      <form
-        onSubmit={(e) => {
-          props.onSubmitVanilla(
-            e,
-            `https://swapi.dev/api/people/?search=${props.currentSearchTerm}`
-          );
-          props.updateNext(null);
-          props.updatePrevious(null);
-        }}>
-        <input
-          type='text'
-          onChange={(e) => handleChangeSearchTerm(e.target.value)}
-          placeholder='Search Your favorite Characters here!'
-          value={props.currentSearchTerm}
-        />
-        <button type='submit'>Submit</button>
-      </form>
-    );
-  }
-  if (props.axiosOrVanilla === 'axios') {
-    return (
-      <form
-        onSubmit={(e) => {
-          props.onSubmitAxios(
-            e,
-            `https://swapi.dev/api/people/?search=${props.currentSearchTerm}`
-          );
-          props.updateNext(null);
-          props.updatePrevious(null);
-        }}>
-        <input
-          type='text'
-          onChange={(e) => handleChangeSearchTerm(e.target.value)}
-          placeholder='Search Your favorite Characters here!'
-          value={props.currentSearchTerm}
-        />
-        <button type='submit'>Submit</button>
-      </form>
-    );
-  }
-=======
   return (
     <form
       onSubmit={(e) => {
@@ -66,7 +22,6 @@ function InputForm(props) {
       <button type='submit'>Submit</button>
     </form>
   );
->>>>>>> refactor2
 }
 
 export default InputForm;
